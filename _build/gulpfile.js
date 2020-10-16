37233859c702fbd9c353515bb85f3b0ea5dbe0a4
igarashi <igarashi@pickles.tv>
2020-10-17 03:17:50 +0900

    [add] 開発環境構築

---
+++ _build/gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+
+const srcDir = '../_src/';
+const deployDir = '../deploy/';
+const config = {
+    mapDir: '../_maps/',
+    cssSrc: [srcDir + 'sass/**/*.scss'],
+    jsSrc: [srcDir + 'js/**/*.js'],
+    scriptName: 'app.js'
+};
+
+let isDevelop = true;
+
+// CSS
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+gulp.task('css', () => {
+    return gulp.src(config.cssSrc, {sourcemaps: isDevelop})
+        .pipe(plumber({
+            errorHandler: function(err) {
+                console.log(err.messageFormatted);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions', 'ie 11'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(deployDir + 'css', {sourcemaps: config.mapDir}));
+});
+
+// JavaScript
+const concat = require('gulp-concat');
+const uglifyES = require('uglify-es');
+const uglifyComposer = require('gulp-uglify/composer');
+const minify = uglifyComposer(uglifyES, console);
+gulp.task('js', () => {
+    return gulp.src(config.jsSrc, {sourcemaps: isDevelop})
+        .pipe(plumber())
+        .pipe(concat(config.scriptName))
+        .pipe(minify({
+            mangle: {
+                safari10: true
+            }
+        }))
+        .pipe(gulp.dest(deployDir + 'js', {sourcemaps: isDevelop}));
+});
+
+// Watch
+gulp.task('watch', () => {
+    gulp.watch(config.cssSrc, gulp.task('css'));
+    gulp.watch(config.jsSrc, gulp.task('js'));
+});
+
+// release
+gulp.task('release', gulp.series((callback) => {
+    isDevelop = false;
+    return callback();
+}, gulp.parallel('css', 'js')));
+
+// default
+gulp.task('default', gulp.series(gulp.parallel('css', 'js'), 'watch'));
---
+++ deploy/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang=ja>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Othello</title>
+    <link rel="stylesheet" href="./css/style.css?v=<?php echo time(); ?>">
+</head>
+<body class="othello">
+
+
+
+<script src="./js/app.js?v=<?php echo time(); ?>"></script>
+</body>
+</html>
